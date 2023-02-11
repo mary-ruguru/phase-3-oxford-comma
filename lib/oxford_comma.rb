27cf0e3ca9d942e16fb822f0ba18148228d6351f
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-02-12 02:09:31 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
 
+    long = array.size
+    case 
+        when long == 1
+            return array.join
+        when long == 2
+            lastelem = array.pop
+            array.push(" and ")
+            array.push (lastelem)
+            return array.join
+        when long >=3
+        
+            lastelem = array.pop  
+            array.push("and ")
+            cad = array.join(', ')
+            cad = cad + lastelem
+            return cad
+           # array.push (lastelem)  
+            #return array
+    end
 end
